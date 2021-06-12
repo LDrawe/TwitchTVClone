c23af6af9e3c439251fd750a60096d4f4ddecc5e
LDrawe <eduardomportilho@hotmail.com>
2021-06-11 23:12:35 -0300

    Bottom Tab Navigation

--- App.tsx
+++ App.tsx
@@ -1,21 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Home from './src/screens/Home';
+import Settings from './src/screens/Settings';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<NavigationContainer>
+			<Tab.Navigator>
+				<Tab.Screen name="Home" component={Home} />
+				<Tab.Screen name="Settings" component={Settings} />
+			</Tab.Navigator>
+		</NavigationContainer>
+	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
